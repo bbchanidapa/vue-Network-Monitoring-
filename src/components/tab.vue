85a15da65725d8475c56580ebc0e619516b3796f
bkbest90 <wthnfbest@gmail.com>
2017-04-30 00:57:30 +0700

    Change interface

--- src/components/tab.vue
+++ src/components/tab.vue
@@ -1,21 +1,26 @@
 <template>
 
-<nav style="background-color: white;">   
-   <div ><img src="http://upic.me/i/ho/fitmlogoencolor.png" width="105px" >
-      <a href="#" class="brand-logo" style="margin-left:50px;"><font color="black">FITM Monitoring v1.0</font></a>   
-      <ul id="nav-mobile" class="right hide-on-med-and-down" style="margin-right:50px;">
-      <li><router-link to="dashboard"><font color="black">Dashboard</font></router-link></li>
-      <li><router-link to="devices"><font color="black">Devices</font></router-link></li>
-      <li><router-link to="interface"><font color="black">Interface</font></router-link></li>
-      <li><router-link to="top10rank"><font color="black">Top 10 ranking</font></router-link></li>
-      </ul>
-   </div>
- </nav>
+  <nav>
+    <div class="nav-wrapper  light-blue darken-4">
+      <div class="container">
+            <a href="#" class="brand-logo">FITM Monitoring v1.0</a>
+        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down ">
+          <li><router-link :to="{ path: 'dashboard' }">Dashboard</router-link></li>
+          <li><router-link :to="{ path: 'devices' }">Device</router-link></li>
+        </ul>
+        <ul class="side-nav" id="mobile-demo">
+          <li><router-link :to="{ path: 'dashboard' }">Dashboard</router-link></li>
+          <li><router-link :to="{ path: 'devices' }">Device</router-link></li>
+        </ul>
+      </div>
+    </div>
+</nav>
 
 </template>
 <script type="text/javascript">
 
 </script>
 <style type="text/css">
-	
+
 </style>
