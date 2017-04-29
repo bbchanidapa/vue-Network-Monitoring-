a95b328a7b2a52843b0b266a7e282166ef8ed2ec
10.10 <10.10>
2017-04-29 23:05:28 +0700

    edit interface

--- src/components/Interface.vue
+++ src/components/Interface.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
-  <p class="redfont">{{msg}}</p>
-  </div>
+ <div >
+ 
+  	<p class="redfont">{{msg}}</p>
+  	
+    	<div class="centre" style="background-color:green;"><center>{{name}}</center>
+          <center><img src="http://www.ryc.cl/carro/files/1165970.jpg" width="90%"></center>
+    	</div>
+    	<div class="bottom" style="background-color:gold;">Graph</div>
+   
+  
+ </div>
 </template>
-
 <script>
 /*eslint-disable */
 export default {
   data () {
     return {
       msg: 'This page is interface page',
+      name: 'Sw4505'
     }
   }
 }
@@ -18,5 +26,23 @@ export default {
 <style type="text/css">
 .redfont {
 	color: red;
+}
+div.centre
+{
+  text-align: left;
+  width: 70%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+div.bottom
+{
+  text-align: left;
+  width: 70%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 100px;
+  margin-top: 100px;
 }
 </style>
--- src/components/tab.vue
+++ src/components/tab.vue
@@ -1,19 +1,15 @@
 <template>
 
-<nav style="background-color: #00f1f1;">   
-    <div >
-      <a href="#" class="brand-logo" style="margin-left:50px;">FITM Monitoring v1.0</a>
-      
-   	 </div>
-   	 <div>
-   	  <ul id="nav-mobile" class="right hide-on-med-and-down" style="margin-right:50px;">
-      <li><router-link to="dashboard">Dashboard</router-link></li>
-      <li><router-link to="devices">Devices</router-link></li>
-      <li><router-link to="interface">Interface</router-link></li>
-      <li><router-link to="top10rank">Top 10 ranking</router-link></li>
+<nav style="background-color: white;">   
+   <div ><img src="http://upic.me/i/ho/fitmlogoencolor.png" width="105px" >
+      <a href="#" class="brand-logo" style="margin-left:50px;"><font color="black">FITM Monitoring v1.0</font></a>   
+      <ul id="nav-mobile" class="right hide-on-med-and-down" style="margin-right:50px;">
+      <li><router-link to="dashboard"><font color="black">Dashboard</font></router-link></li>
+      <li><router-link to="devices"><font color="black">Devices</font></router-link></li>
+      <li><router-link to="interface"><font color="black">Interface</font></router-link></li>
+      <li><router-link to="top10rank"><font color="black">Top 10 ranking</font></router-link></li>
       </ul>
-     </div>
-
+   </div>
  </nav>
 
 </template>
